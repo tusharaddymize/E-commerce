2c6ebd19979765040d49a036936d06cef3106314
Tushar Raghav <tushar@addymize.in>
2026-08-07 14:33:02 +0530

    Debug CLIENT_URL

--- server/server.js
+++ server/server.js
@@ -42,6 +42,20 @@ connectDB();
 // Middlewares
 // =============================================
 
+console.log("CLIENT_URL:", process.env.CLIENT_URL);
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      process.env.CLIENT_URL,
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: [
